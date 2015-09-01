84c3d46e0a1ab35f5c35156867ab445627cbb4cf
Pascal Weisenburger <pascal.weisenburger@web.de>
2015-09-01 03:16:35 +0200

    extended typer with method to clean modifier flags from problematic cases and extended type AST creation (the latter may not be needed if all types can be inferred in the end)

--- retier-core/src/main/scala/retier/impl/engine/Typer.scala
+++ retier-core/src/main/scala/retier/impl/engine/Typer.scala
@@ -90,6 +90,31 @@ class Typer[C <: Context](val c: C) {
         fixTypecheck(tree, abortWhenUnfixable = true))
 
   /**
+   * Cleans the flag set of the given modifiers.
+   *
+   * The type-checking process annotates definitions with various flags. Some
+   * of them can also be inserted by user-code or even have a corresponding
+   * Scala language construct, but others are only used by the type-checker.
+   * Certain flags can interfere with type-checking and cause it to fail. Those
+   * flags can be safely removed and will be re-inserted during type-checking
+   * when needed.
+   *
+   * This method eliminates some problematic cases.
+   */
+  def cleanModifiers(mods: Modifiers): Modifiers = {
+    val possibleFlags = Seq(ABSTRACT, ARTIFACT, BYNAMEPARAM, CASE, CASEACCESSOR,
+      CONTRAVARIANT, COVARIANT, DEFAULTINIT, DEFAULTPARAM, DEFERRED, ENUM,
+      FINAL, IMPLICIT, LAZY, LOCAL, MACRO, MUTABLE, OVERRIDE, PARAM,
+      PARAMACCESSOR, PRESUPER, PRIVATE, PROTECTED, SEALED, SYNTHETIC)
+
+    val flags = possibleFlags.fold(NoFlags) { (flags, flag) =>
+      if (mods hasFlag flag) flags | flag else flags
+    }
+    
+    Modifiers(flags, mods.privateWithin, mods.annotations)
+  }
+
+  /**
    * Creates an AST representing the given type.
    *
    * The type-checking process creates synthetic type trees and it is possible
@@ -100,6 +125,9 @@ class Typer[C <: Context](val c: C) {
    * type-checking and un-type-checking.
    */
   def createTypeTree(tpe: Type): Tree = {
+    def isClass(symbol: Symbol): Boolean =
+      symbol.isClass && !symbol.isModule && !symbol.isPackage
+
     def expandSymbol(symbol: Symbol): Tree = {
       if (symbol.owner != NoSymbol)
         Select(expandSymbol(symbol.owner), symbol.name.toTermName)
@@ -108,24 +136,46 @@ class Typer[C <: Context](val c: C) {
     }
 
     def expandType(tpe: Type): Tree = tpe match {
-      case tpe @ TypeRef(pre, sym, args) =>
-        val tpt = pre match {
-          case ThisType(pre) if pre.isModule || pre.isPackage =>
-            Some(Select(expandSymbol(pre), sym.asType.name))
-          case ThisType(pre) if pre.isClass =>
-            Some(Select(This(pre.asType.name), sym.asType.name))
-          case _ =>
-            None
+      case ThisType(pre) if isClass(pre) =>
+        This(pre.asType.name)
+
+      case ThisType(pre) =>
+        expandSymbol(pre)
+
+      case TypeRef(NoPrefix, sym, args) =>
+        val ident = Ident(sym.name.toTypeName)
+        if (!args.isEmpty)
+          AppliedTypeTree(ident, args map expandType)
+        else
+          ident
+
+      case TypeRef(pre, sym, args) =>
+        val preTree = expandType(pre)
+
+        val typeProjection = preTree match {
+          case This(_) => false
+          case _ => isClass(pre.typeSymbol)
         }
 
-        tpt match {
-          case Some(tpt) if args.isEmpty =>
-            tpt
-          case Some(tpt) =>
-            AppliedTypeTree(tpt, args map expandType)
-          case _ =>
-            TypeTree(tpe)
+        val select =
+          if (typeProjection) SelectFromTypeTree(preTree, sym.name.toTypeName)
+          else Select(preTree, sym.name.toTypeName)
+
+        if (!args.isEmpty)
+          AppliedTypeTree(select, args map expandType)
+        else
+          select
+
+      case SingleType(NoPrefix, sym) =>
+        SingletonTypeTree(Ident(sym.name.toTermName))
+
+      case SingleType(pre, sym) =>
+        val preTree = expandType(pre) match {
+          case SingletonTypeTree(pre) => pre
+          case pre => pre
         }
+
+        SingletonTypeTree(Select(preTree, sym.name.toTermName))
 
       case _ =>
         TypeTree(tpe)
@@ -190,10 +240,6 @@ class Typer[C <: Context](val c: C) {
 
 
   private def fixTypecheck(tree: Tree, abortWhenUnfixable: Boolean): Tree = {
-    val possibleFlags = Seq(
-      ABSTRACT, FINAL, IMPLICIT, LAZY, LOCAL,
-      OVERRIDE, PRIVATE, PROTECTED, SEALED)
-
     val rhss = (tree collect {
       case valDef @ ValDef(_, _, _, _) if valDef.symbol.isTerm =>
         val term = valDef.symbol.asTerm
@@ -239,9 +285,8 @@ class Typer[C <: Context](val c: C) {
               !term.isLazy && term.isGetter && (rhss contains term)
             } =>
           val valDef = rhss(tree.symbol)
-          val flags = possibleFlags.fold(NoFlags) { (flags, flag) =>
-            if (mods hasFlag flag) flags | flag else flags
-          } | (if (valDef.symbol.asTerm.isVar) MUTABLE else NoFlags)
+          val flags = cleanModifiers(mods).flags |
+            (if (valDef.symbol.asTerm.isVar) MUTABLE else NoFlags)
           val privateWithin =
             if (defDef.symbol.asTerm.privateWithin != NoSymbol)
               defDef.symbol.asTerm.privateWithin.name
@@ -266,9 +311,7 @@ class Typer[C <: Context](val c: C) {
             case rhs :: _ => rhs
             case _ => rhs
           }
-          val flags = possibleFlags.fold(NoFlags) { (flags, flag) =>
-            if (mods hasFlag flag) flags | flag else flags
-          }
+          val flags = cleanModifiers(mods).flags
           val privateWithin =
             if (defDef.symbol.asTerm.privateWithin != NoSymbol)
               defDef.symbol.asTerm.privateWithin.name
