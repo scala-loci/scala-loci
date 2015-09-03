27f41f61785f8a58c9ac92c5701240e38b7b6e36
Pascal Weisenburger <pascal.weisenburger@web.de>
2015-09-04 01:58:35 +0200

    suppress (consequential) error due to missing peer type tag when the compilation run already has errors

--- retier-core/src/main/scala/retier/impl/PeerType.scala
+++ retier-core/src/main/scala/retier/impl/PeerType.scala
@@ -6,10 +6,10 @@ import scala.reflect.ClassTag
 import scala.reflect.macros.whitebox.Context
 
 private final case class PeerTypeImpl(name: String, bases: List[PeerType])
-    extends PeerTypeImplBase
+  extends PeerTypeImplBase
 
 private final case class PeerTypeTagImpl[P](peerType: PeerTypeImpl)
-    extends PeerTypeTagImplBase[P]
+  extends PeerTypeTagImplBase[P]
 
 object PeerTypeTag {
   def create[P](name: String, bases: List[PeerType]): PeerTypeTag[P] =
@@ -24,28 +24,30 @@ object PeerTypeTag {
       (c: Context)(ev: c.Expr[ClassTag[P]]): c.Expr[PeerTypeTag[P]] = {
     import c.universe._
 
-    val enclosingMultitierMacro = c.enclosingMacros exists { c =>
-      import c.universe._
-      val multitierSymbol = c.mirror.staticClass("_root_.retier.multitier")
+    if (!c.hasErrors) {
+      val enclosingMultitierMacro = c.enclosingMacros exists { c =>
+        import c.universe._
+        val multitierSymbol = c.mirror.staticClass("_root_.retier.multitier")
 
-      c.macroApplication match {
-        case q"new $expr[..$tpts](...$exprss).macroTransform[..$_](...$_)" =>
-          val annotation = q"new $expr[..$tpts](...$exprss)"
-          (c typecheck (annotation, silent = true)) match {
-            case q"new $expr[..$_](...$_)" =>
-              expr.symbol == multitierSymbol
-            case _ =>
-              false
-          }
-        case _ =>
-          false
+        c.macroApplication match {
+          case q"new $expr[..$tpts](...$exprss).macroTransform[..$_](...$_)" =>
+            val annotation = q"new $expr[..$tpts](...$exprss)"
+            (c typecheck (annotation, silent = true)) match {
+              case q"new $expr[..$_](...$_)" =>
+                expr.symbol == multitierSymbol
+              case _ =>
+                false
+            }
+          case _ =>
+            false
+        }
       }
-    }
 
-    if (!enclosingMultitierMacro)
-      c.abort(c.enclosingPosition,
-        "Removing macro from the list of implicit candidates " +
-        "since there is no enclosing `multitier` macro being expanded")
+      if (!enclosingMultitierMacro)
+        c.abort(c.enclosingPosition,
+          "Removing macro from the list of implicit candidates " +
+          "since there is no enclosing `multitier` macro being expanded")
+    }
 
     c.Expr[PeerTypeTag[P]](
       q"_root_.retier.impl.PeerTypeTag.dummy[${weakTypeOf[P]}]")
