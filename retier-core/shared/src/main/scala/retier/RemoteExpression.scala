scala-loci/scala-loci

commit 08b2930442508ec1f39de395b390cf147f88a414

make `Nothing on P` inferable; IntelliJ workarounds for `remote.on` expressions

retier-core/shared/src/main/scala/retier/RemoteExpression.scala
@@ -4,8 +4,10 @@ import dslparadise._
 import scala.language.higherKinds
 
 protected final abstract class RemoteSelectionExpression[P <: Peer] {
-  def on[P0 <: P](peer: Remote[P0]): RemoteExpression[P, fromSingle]
-  def on[P0 <: P](peers: Remote[P0]*): RemoteExpression[P, fromMultiple]
+  def on[P0 <: P, `P'` <: Peer](peer: Remote[P0])
+    (implicit ev: FirstIfNotEmptyElseSecond[P, P0, `P'`]): RemoteExpression[`P'`, fromSingle]
+  def on[P0 <: P, `P'` <: Peer](peers: Remote[P0]*)
+    (implicit ev: FirstIfNotEmptyElseSecond[P, P0, `P'`]): RemoteExpression[`P'`, fromMultiple]
 }
 
 protected final abstract class RemoteExpression[P <: Peer, placed[_, _ <: Peer]] {

retier-core/shared/src/main/scala/retier/SupportTypes.scala
@@ -6,7 +6,12 @@ import scala.annotation.implicitNotFound
 @implicitNotFound("Expression not placeable on peer")
 protected final abstract class PlacingTypes[P <: Peer, T, U]
 
-protected object PlacingTypes {
+protected trait PlacingTypesFallback {
+  implicit def nothingOrNotInferred[P <: Peer, T, NothingButLessSpecific]:
+    PlacingTypes[P, T, NothingButLessSpecific] = `#macro`
+}
+
+protected object PlacingTypes extends PlacingTypesFallback {
   implicit def localPlacedType[P <: Peer, P0 <: Peer, T, U]
     (implicit
         ev0: T <:< (U localOn P0),

commit 2e7a51d002e3d400d813ab82262547c7c7a877d7

fixed issuing nested remote computation to a specific peer instance without capturing (problem introduced in 08b2930)

retier-core/shared/src/main/scala/retier/RemoteExpression.scala
@@ -4,10 +4,8 @@ import dslparadise._
 import scala.language.higherKinds
 
 protected final abstract class RemoteSelectionExpression[P <: Peer] {
-  def on[P0 <: P, `P'` <: Peer](peer: Remote[P0])
-    (implicit ev: FirstIfNotEmptyElseSecond[P, P0, `P'`]): RemoteExpression[`P'`, fromSingle]
-  def on[P0 <: P, `P'` <: Peer](peers: Remote[P0]*)
-    (implicit ev: FirstIfNotEmptyElseSecond[P, P0, `P'`]): RemoteExpression[`P'`, fromMultiple]
+  def on[P0 <: P](peer: Remote[P0]): RemoteExpression[P, fromSingle]
+  def on[P0 <: P](peers: Remote[P0]*): RemoteExpression[P, fromMultiple]
 }
 
 protected final abstract class RemoteExpression[P <: Peer, placed[_, _ <: Peer]] {