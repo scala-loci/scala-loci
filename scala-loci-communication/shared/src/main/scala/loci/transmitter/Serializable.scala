c42e7060badbc21ba7f698e26914f391667a877a
Pascal Weisenburger <pascal.weisenburger@web.de>
2019-07-13 14:38:20 +0200

    improve error message for unresolved serializers

--- scala-loci-communication/shared/src/main/scala/loci/transmitter/Serializable.scala
+++ scala-loci-communication/shared/src/main/scala/loci/transmitter/Serializable.scala
@@ -2,10 +2,36 @@ package loci
 package transmitter
 
 import scala.util.Try
-import scala.annotation.implicitNotFound
+import scala.annotation.{compileTimeOnly, implicitNotFound}
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox
 
 @implicitNotFound("${T} is not serializable")
 trait Serializable[T] {
   def serialize(value: T): MessageBuffer
   def deserialize(value: MessageBuffer): Try[T]
+}
+
+object Serializable {
+  @compileTimeOnly("Value is not serializable")
+  final implicit def resolutionFailure[T]: Serializable[T] =
+    macro SerializableResolutionFailure[T]
+
+  @compileTimeOnly("Value is not serializable")
+  final def dummy[T]: Serializable[T] = throw new NotImplementedError
+}
+
+object SerializableResolutionFailure {
+  def apply[T: c.WeakTypeTag](c: blackbox.Context): c.Tree = {
+    import c.universe._
+
+    val tpe = weakTypeOf[T]
+    val message = s"$tpe is not serializable"
+
+    q"""{
+      @${termNames.ROOTPKG}.scala.annotation.compileTimeOnly($message) def resolutionFailure() = ()
+      resolutionFailure()
+      ${termNames.ROOTPKG}.loci.transmitter.Serializable.dummy[$tpe]
+    }"""
+  }
 }
