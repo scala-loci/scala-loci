469e64ee29cff6447bef9c72492bf014fe2e5902
Pascal Weisenburger <pascal.weisenburger@web.de>
2021-08-01 14:40:49 +0200

    improve type inference for placed types

--- scala-loci-lang/shared/src/main/scala/loci/language/PlacedType.scala
+++ scala-loci-lang/shared/src/main/scala/loci/language/PlacedType.scala
@@ -40,7 +40,85 @@ sealed trait PlacedCleanDefault extends PlacedCleanFallback {
   implicit def default[V, L, T]: PlacedClean[V, L, T, T, T] = erased
 }
 
-sealed trait PlacedCleanHigherKind8 extends PlacedCleanDefault {
+sealed trait PlacedCleanHigherKind1 extends PlacedCleanDefault {
+  implicit def higherKind1[V, L, T[_], T_, T0, U0]
+    (implicit
+        ev0: T[T0] =:= T_,
+        ev1: PlacedClean[V, L, T0, T0, U0])
+    : PlacedClean[V, L, T[T0], T_, T[U0]] = erased(ev0, ev1)
+}
+
+sealed trait PlacedCleanHigherKind2 extends PlacedCleanHigherKind1 {
+  implicit def higherKind2[V, L, T[_, _], T_, T0, T1, U0, U1]
+    (implicit
+        ev0: T[T0, T1] =:= T_,
+        ev1: PlacedClean[V, L, T0, T0, U0],
+        ev2: PlacedClean[V, L, T1, T1, U1])
+    : PlacedClean[V, L, T[T0, T1], T_, T[U0, U1]] = erased(ev0, ev1, ev2)
+}
+
+sealed trait PlacedCleanHigherKind3 extends PlacedCleanHigherKind2 {
+  implicit def higherKind3[V, L, T[_, _, _], T_, T0, T1, T2, U0, U1, U2]
+    (implicit
+        ev0: T[T0, T1, T2] =:= T_,
+        ev1: PlacedClean[V, L, T0, T0, U0],
+        ev2: PlacedClean[V, L, T1, T1, U1],
+        ev3: PlacedClean[V, L, T2, T2, U2])
+    : PlacedClean[V, L, T[T0, T1, T2], T_, T[U0, U1, U2]] = erased(ev0, ev1, ev2, ev3)
+}
+
+sealed trait PlacedCleanHigherKind4 extends PlacedCleanHigherKind3 {
+  implicit def higherKind4[V, L, T[_, _, _, _], T_, T0, T1, T2, T3, U0, U1, U2, U3]
+    (implicit
+        ev0: T[T0, T1, T2, T3] =:= T_,
+        ev1: PlacedClean[V, L, T0, T0, U0],
+        ev2: PlacedClean[V, L, T1, T1, U1],
+        ev3: PlacedClean[V, L, T2, T2, U2],
+        ev4: PlacedClean[V, L, T3, T3, U3])
+    : PlacedClean[V, L, T[T0, T1, T2, T3], T_, T[U0, U1, U2, U3]] = erased(ev0, ev1, ev2, ev3, ev4)
+}
+
+sealed trait PlacedCleanHigherKind5 extends PlacedCleanHigherKind4 {
+  implicit def higherKind5[V, L, T[_, _, _, _, _], T_, T0, T1, T2, T3, T4, U0, U1, U2, U3, U4]
+    (implicit
+        ev0: T[T0, T1, T2, T3, T4] =:= T_,
+        ev1: PlacedClean[V, L, T0, T0, U0],
+        ev2: PlacedClean[V, L, T1, T1, U1],
+        ev3: PlacedClean[V, L, T2, T2, U2],
+        ev4: PlacedClean[V, L, T3, T3, U3],
+        ev5: PlacedClean[V, L, T4, T4, U4])
+    : PlacedClean[V, L, T[T0, T1, T2, T3, T4], T_, T[U0, U1, U2, U3, U4]] = erased(ev0, ev1, ev2, ev3, ev4, ev5)
+}
+
+
+sealed trait PlacedCleanHigherKind6 extends PlacedCleanHigherKind5 {
+  implicit def higherKind6[V, L, T[_, _, _, _, _, _], T_, T0, T1, T2, T3, T4, T5, U0, U1, U2, U3, U4, U5]
+    (implicit
+        ev0: T[T0, T1, T2, T3, T4, T5] =:= T_,
+        ev1: PlacedClean[V, L, T0, T0, U0],
+        ev2: PlacedClean[V, L, T1, T1, U1],
+        ev3: PlacedClean[V, L, T2, T2, U2],
+        ev4: PlacedClean[V, L, T3, T3, U3],
+        ev5: PlacedClean[V, L, T4, T4, U4],
+        ev6: PlacedClean[V, L, T5, T5, U5])
+    : PlacedClean[V, L, T[T0, T1, T2, T3, T4, T5], T_, T[U0, U1, U2, U3, U4, U5]] = erased(ev0, ev1, ev2, ev3, ev4, ev5, ev6)
+}
+
+sealed trait PlacedCleanHigherKind7 extends PlacedCleanHigherKind6 {
+  implicit def higherKind7[V, L, T[_, _, _, _, _, _, _], T_, T0, T1, T2, T3, T4, T5, T6, U0, U1, U2, U3, U4, U5, U6]
+    (implicit
+        ev0: T[T0, T1, T2, T3, T4, T5, T6] =:= T_,
+        ev1: PlacedClean[V, L, T0, T0, U0],
+        ev2: PlacedClean[V, L, T1, T1, U1],
+        ev3: PlacedClean[V, L, T2, T2, U2],
+        ev4: PlacedClean[V, L, T3, T3, U3],
+        ev5: PlacedClean[V, L, T4, T4, U4],
+        ev6: PlacedClean[V, L, T5, T5, U5],
+        ev7: PlacedClean[V, L, T6, T6, U6])
+    : PlacedClean[V, L, T[T0, T1, T2, T3, T4, T5, T6], T_, T[U0, U1, U2, U3, U4, U5, U6]] = erased(ev0, ev1, ev2, ev3, ev4, ev5, ev6, ev7)
+}
+
+sealed trait PlacedCleanHigherKind8 extends PlacedCleanHigherKind7 {
   implicit def higherKind8[V, L, T[_, _, _, _, _, _, _, _], T_, T0, T1, T2, T3, T4, T5, T6, T7, U0, U1, U2, U3, U4, U5, U6, U7]
     (implicit
         ev0: T[T0, T1, T2, T3, T4, T5, T6, T7] =:= T_,
@@ -55,84 +133,7 @@ sealed trait PlacedCleanHigherKind8 extends PlacedCleanDefault {
     : PlacedClean[V, L, T[T0, T1, T2, T3, T4, T5, T6, T7], T_, T[U0, U1, U2, U3, U4, U5, U6, U7]] = erased(ev0, ev1, ev2, ev3, ev4, ev5, ev6, ev7, ev8)
 }
 
-sealed trait PlacedCleanHigherKind7 extends PlacedCleanHigherKind8 {
-  implicit def higherKind7[V, L, T[_, _, _, _, _, _, _], T_, T0, T1, T2, T3, T4, T5, T6, U0, U1, U2, U3, U4, U5, U6]
-    (implicit
-        ev0: T[T0, T1, T2, T3, T4, T5, T6] =:= T_,
-        ev1: PlacedClean[V, L, T0, T0, U0],
-        ev2: PlacedClean[V, L, T1, T1, U1],
-        ev3: PlacedClean[V, L, T2, T2, U2],
-        ev4: PlacedClean[V, L, T3, T3, U3],
-        ev5: PlacedClean[V, L, T4, T4, U4],
-        ev6: PlacedClean[V, L, T5, T5, U5],
-        ev7: PlacedClean[V, L, T6, T6, U6])
-    : PlacedClean[V, L, T[T0, T1, T2, T3, T4, T5, T6], T_, T[U0, U1, U2, U3, U4, U5, U6]] = erased(ev0, ev1, ev2, ev3, ev4, ev5, ev6, ev7)
-}
-
-sealed trait PlacedCleanHigherKind6 extends PlacedCleanHigherKind7 {
-  implicit def higherKind6[V, L, T[_, _, _, _, _, _], T_, T0, T1, T2, T3, T4, T5, U0, U1, U2, U3, U4, U5]
-    (implicit
-        ev0: T[T0, T1, T2, T3, T4, T5] =:= T_,
-        ev1: PlacedClean[V, L, T0, T0, U0],
-        ev2: PlacedClean[V, L, T1, T1, U1],
-        ev3: PlacedClean[V, L, T2, T2, U2],
-        ev4: PlacedClean[V, L, T3, T3, U3],
-        ev5: PlacedClean[V, L, T4, T4, U4],
-        ev6: PlacedClean[V, L, T5, T5, U5])
-    : PlacedClean[V, L, T[T0, T1, T2, T3, T4, T5], T_, T[U0, U1, U2, U3, U4, U5]] = erased(ev0, ev1, ev2, ev3, ev4, ev5, ev6)
-}
-
-sealed trait PlacedCleanHigherKind5 extends PlacedCleanHigherKind6 {
-  implicit def higherKind5[V, L, T[_, _, _, _, _], T_, T0, T1, T2, T3, T4, U0, U1, U2, U3, U4]
-    (implicit
-        ev0: T[T0, T1, T2, T3, T4] =:= T_,
-        ev1: PlacedClean[V, L, T0, T0, U0],
-        ev2: PlacedClean[V, L, T1, T1, U1],
-        ev3: PlacedClean[V, L, T2, T2, U2],
-        ev4: PlacedClean[V, L, T3, T3, U3],
-        ev5: PlacedClean[V, L, T4, T4, U4])
-    : PlacedClean[V, L, T[T0, T1, T2, T3, T4], T_, T[U0, U1, U2, U3, U4]] = erased(ev0, ev1, ev2, ev3, ev4, ev5)
-}
-
-sealed trait PlacedCleanHigherKind4 extends PlacedCleanHigherKind5 {
-  implicit def higherKind4[V, L, T[_, _, _, _], T_, T0, T1, T2, T3, U0, U1, U2, U3]
-    (implicit
-        ev0: T[T0, T1, T2, T3] =:= T_,
-        ev1: PlacedClean[V, L, T0, T0, U0],
-        ev2: PlacedClean[V, L, T1, T1, U1],
-        ev3: PlacedClean[V, L, T2, T2, U2],
-        ev4: PlacedClean[V, L, T3, T3, U3])
-    : PlacedClean[V, L, T[T0, T1, T2, T3], T_, T[U0, U1, U2, U3]] = erased(ev0, ev1, ev2, ev3, ev4)
-}
-
-sealed trait PlacedCleanHigherKind3 extends PlacedCleanHigherKind4 {
-  implicit def higherKind3[V, L, T[_, _, _], T_, T0, T1, T2, U0, U1, U2]
-    (implicit
-        ev0: T[T0, T1, T2] =:= T_,
-        ev1: PlacedClean[V, L, T0, T0, U0],
-        ev2: PlacedClean[V, L, T1, T1, U1],
-        ev3: PlacedClean[V, L, T2, T2, U2])
-    : PlacedClean[V, L, T[T0, T1, T2], T_, T[U0, U1, U2]] = erased(ev0, ev1, ev2, ev3)
-}
-
-sealed trait PlacedCleanHigherKind2 extends PlacedCleanHigherKind3 {
-  implicit def higherKind2[V, L, T[_, _], T_, T0, T1, U0, U1]
-    (implicit
-        ev0: T[T0, T1] =:= T_,
-        ev1: PlacedClean[V, L, T0, T0, U0],
-        ev2: PlacedClean[V, L, T1, T1, U1])
-    : PlacedClean[V, L, T[T0, T1], T_, T[U0, U1]] = erased(ev0, ev1, ev2)
-}
-
-sealed trait PlacedCleanHigherKind1 extends PlacedCleanHigherKind2 {
-  implicit def higherKind1[V, L, T[_], T_, T0, U0]
-    (implicit
-        ev0: T[T0] =:= T_,
-        ev1: PlacedClean[V, L, T0, T0, U0])
-    : PlacedClean[V, L, T[T0], T_, T[U0]] = erased(ev0, ev1)
-}
-
-sealed trait PlacedCleanRemotePeerSelection extends PlacedCleanHigherKind1 {
+sealed trait PlacedCleanRemotePeerSelection extends PlacedCleanHigherKind8 {
   implicit def remotePeerSelection[V, L, T, P]
     : PlacedClean[V, L, T from P, T from P, Unit] = erased
   implicit def remotePeerSelectionSingle[V, L, T, P]
@@ -152,8 +153,11 @@ sealed trait PlacedCleanLocalPeer extends PlacedCleanRemotePeer {
 }
 
 sealed trait PlacedCleanLocalPeerLocal extends PlacedCleanLocalPeer {
-  implicit def localPeerLocal[V, L <: P, T, U, P, _Local_[T] <: Local[T]](implicit ev: PlacedClean[V, L, T, T, U])
-    : PlacedClean[V, L, _Local_[T] on P, _Local_[T] on P, U] = erased(ev)
+  implicit def localPeerLocal[V, L <: P, T, U, P, _Local_[T] <: Local[T]]
+    (implicit
+        ev0: _Local_[T] =:= T,
+        ev1: PlacedClean[V, L, T, T, U])
+    : PlacedClean[V, L, _Local_[T] on P, _Local_[T] on P, U] = erased(ev0, ev1)
 }
 
 sealed trait PlacedCleanSubjective extends PlacedCleanLocalPeerLocal {
