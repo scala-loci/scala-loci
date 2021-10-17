7f6dc6aeaaf6a3bab63c7e6c48acb6d6f0799a0d
Lukas Lehmann <lukas.lehmann97@gmail.com>
2021-10-17 12:01:43 +0200

    add test cases for union peer types

---
+++ scala-loci-lang/shared/src/test/scala/loci/language/impl/UnionPeerTypeSpec.scala
@@ -0,0 +1,72 @@
+package loci
+package language
+package impl
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class UnionPeerTypeSpec extends AnyFlatSpec with Matchers with NoLogging {
+  behavior of "Union peer types"
+
+  it should "compile for a simple union of two peers" in {
+    """@multitier object Module {
+      @peer type A <: { type Tie <: Multiple[B] }
+      @peer type B
+
+      def f(x: Int): Int on (A | B) = on[A] { implicit! =>
+        x + 1
+      } and on[B] { implicit! =>
+        x + 2
+      }
+    }""" should compile
+  }
+
+  it should "compile for a complex union of multiple peers and peergroups" in {
+    """@multitier object Module {
+      @peer type A <: { type Tie <: Multiple[B] }
+      @peer type B
+      @peergroup type T
+      @peer type C <: T
+      @peer type D <: T { type Tie <: Single[T] }
+      @peergroup type U
+      @peer type E <: U
+      @peer type F <: U
+
+
+      def f(x: Int): Int on (A | B | T | U) = on[A | T] { implicit! =>
+        x + 1
+      } and on[B | U] { implicit! =>
+        x + 2
+      }
+    }""" should compile
+  }
+
+  it should "not compile for a missing implementation of a peer of the union" in {
+    """@multitier object Module {
+      @peer type A <: { type Tie <: Multiple[B] }
+      @peer type B
+      @peer type C
+
+      def f(x: Int): Int on (A | B | C) = on[A | B] { implicit! =>
+        x + 1
+      }
+    }""" shouldNot compile
+  }
+
+  it should "not compile for an implementation of a peer that is not part of the union" in {
+    """@multitier object Module {
+      @peer type A <: { type Tie <: Multiple[B] }
+      @peer type B
+      @peer type C
+
+      def f(x: Int): Int on (A | B) = on[A] { implicit! =>
+        x + 1
+      } and on[B] { implicit! =>
+        x + 2
+      } and on[C] { implicit! =>
+        x + 2
+      }
+    }""" shouldNot compile
+  }
+
+}
