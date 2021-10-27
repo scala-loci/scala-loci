a5ba0868238e510bfd31b11468e81c2587ba5f54
Lukas Lehmann <lukas.lehmann97@gmail.com>
2021-10-27 16:56:47 +0200

    test that unique peer ids are unique

---
+++ scala-loci-lang/shared/src/test/scala/loci/concepts/UniquePeerIdSpec.scala
@@ -0,0 +1,28 @@
+package loci
+package concepts
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox
+
+@multitier object UniquePeerIdModule {
+  @peer type Node
+}
+
+class UniquePeerIdSpec extends AnyFlatSpec with Matchers with NoLogging {
+  behavior of "Unique peer id"
+
+  it should "generate different ids on different instances" in {
+    val nodeA = multitier start new Instance[UniquePeerIdModule.Node](contexts.Immediate.global)
+    val nodeB = multitier start new Instance[UniquePeerIdModule.Node](contexts.Immediate.global)
+
+    val idA = nodeA.instance.current.map(_.retrieveUniquePeerId())
+    val idB = nodeB.instance.current.map(_.retrieveUniquePeerId())
+
+    idA should be(defined)
+    idB should be(defined)
+    idA.get should not equal idB.get
+  }
+
+}
