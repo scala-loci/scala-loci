7e67a8e876e2fb6a9fe7bfe8c62efde37d25f62f
Lukas Lehmann <lukas.lehmann97@gmail.com>
2021-11-04 14:45:36 +0100

    tests for failing value reference accesses

--- scala-loci-lang/shared/src/test/scala/loci/concepts/ValueRefSpec.scala
+++ scala-loci-lang/shared/src/test/scala/loci/concepts/ValueRefSpec.scala
@@ -2,16 +2,20 @@ package loci
 package concepts
 
 import loci.communicator.NetworkListener
+import loci.valueref.ValueRefAccessors.NotConnectedToPeerWithId
+import loci.valueref.ValueRefAccessors.PeerValueCacheMiss
 import loci.valueref._
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
 import transmitter.Serializables._
 
-import scala.concurrent.Future
+import java.util.UUID
+import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 @multitier object ValueRefModule {
-  @peer type Node <: { type Tie <: Single[Node] }
+  @peer type Node <: { type Tie <: Optional[Node] }
 
   def generateRef(x: Int): String via Node on Node = on[Node] { implicit! =>
     val s = s"value $x"
@@ -24,8 +28,10 @@ import scala.concurrent.ExecutionContext.Implicits.global
   }
 }
 
-class ValueRefSpec extends AnyFlatSpec with Matchers with NoLogging {
+class ValueRefSpec extends AsyncFlatSpec with Matchers with NoLogging {
   behavior of "value references"
+
+  override def executionContext: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
 
   it should "generate value references on one node and access them on the other" in {
     val listener = new NetworkListener
@@ -46,14 +52,50 @@ class ValueRefSpec extends AnyFlatSpec with Matchers with NoLogging {
       ref
     }
 
-    (refs zip numbers).reverse.foreach {
+    Future.sequence((refs zip numbers).reverse.flatMap {
       case (ref, x) =>
-        nodeB.instance.current.foreach {
-          _.retrieve(ValueRefModule.accessRef(ref)).map {
-            _ shouldEqual s"value $x"
-          }
+        nodeB.instance.current.map {
+          _.retrieve[Future[String]](ValueRefModule.accessRef(ref)).map { _ -> s"value $x" }
         }
+    }).map(_.unzip).map {
+      case (actual, expected) => actual shouldEqual expected
     }
+  }
+
+  it should "fail when accessing a value reference on an instance that has no connection to the referenced peer" in {
+    val nodeA = multitier start new Instance[ValueRefModule.Node](contexts.Immediate.global)
+    val nodeB = multitier start new Instance[ValueRefModule.Node](contexts.Immediate.global)
+
+    val ref: String via ValueRefModule.Node =
+      nodeA.instance.current.map { _.retrieve(ValueRefModule.generateRef(42)) }.get
+
+    nodeB.instance.current.map {
+      _.retrieve[Future[String]](ValueRefModule.accessRef(ref)).failed.map {
+        _ shouldBe a[NotConnectedToPeerWithId]
+      }
+    }.get
+  }
+
+  it should "fail when accessing a value reference that is not present in the cache of the peer the value should exist on" in {
+    val listener = new NetworkListener
+    val nodeA = multitier start new Instance[ValueRefModule.Node](
+      contexts.Immediate.global,
+      listen[ValueRefModule.Node](listener)
+    )
+    val nodeB = multitier start new Instance[ValueRefModule.Node](
+      contexts.Immediate.global,
+      connect[ValueRefModule.Node](listener.createConnector())
+    )
+
+    val ref: String via ValueRefModule.Node =
+      nodeA.instance.current.map { _.retrieve(ValueRefModule.generateRef(42)) }.get
+    val fakeRef = ref.copy[String, ValueRefModule.Node](valueId = UUID.randomUUID())
+
+    nodeB.instance.current.map {
+      _.retrieve[Future[String]](ValueRefModule.accessRef(fakeRef)).failed.map {
+        _ shouldBe a[PeerValueCacheMiss]
+      }
+    }.get
   }
 
 }
