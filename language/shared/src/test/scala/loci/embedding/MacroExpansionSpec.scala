scala-loci/scala-loci

commit 1fc2f19ec62564658122f91d5f18949b6e99f0ae

dependencies updated

scala-loci-lang/shared/src/main/scala/loci/language/impl/Multitier.scala
@@ -24,8 +24,10 @@ object Multitier {
 
 class Multitier(val c: blackbox.Context) {
   import c.universe._
+  import retypecheck._
 
   val logging = Logging(c)
+  val retyper = c.retyper
 
   def annotation(annottees: Tree*): Tree = {
     val multitierAnnotationType = c.mirror.staticClass("_root_.loci.multitier").toType
@@ -88,17 +90,6 @@ class Multitier(val c: blackbox.Context) {
 
     val (annottee, companion) = annottees match {
       case ClassDef(mods, tpname, tparams, impl @ Template(parents, self, _)) :: companion =>
-        def reducedFlags(mods: Modifiers): Modifiers = {
-          val reducedFlags =
-            Seq(Flag.ARTIFACT, Flag.BYNAMEPARAM, Flag.CONTRAVARIANT, Flag.COVARIANT,
-                Flag.FINAL, Flag.IMPLICIT, Flag.LOCAL, Flag.MUTABLE, Flag.OVERRIDE,
-                Flag.PARAM, Flag.PARAMACCESSOR, Flag.PRIVATE, Flag.PROTECTED, Flag.SYNTHETIC)
-              .foldLeft(NoFlags) { (flagAcc, flag) =>
-                if (mods hasFlag flag) flagAcc | flag else flagAcc
-              }
-          Modifiers(reducedFlags, mods.privateWithin, mods.annotations)
-        }
-
         val body = impl.body map {
           case tree @ DefDef(mods, termNames.CONSTRUCTOR, tparams, _, tpt, rhs) =>
             constructorParams += tree.vparamss
@@ -107,7 +98,7 @@ class Multitier(val c: blackbox.Context) {
               _ map { tree =>
                 val ValDef(mods, name, tpt, rhs) = tree: @unchecked
                 if ((mods hasFlag Flag.DEFAULTPARAM) && rhs.nonEmpty)
-                  treeCopy.ValDef(tree, reducedFlags(mods), name, tpt, EmptyTree)
+                  treeCopy.ValDef(tree, retyper.cleanModifiers(mods), name, tpt, EmptyTree)
                 else
                   tree
               }
@@ -152,7 +143,6 @@ class Multitier(val c: blackbox.Context) {
     val processedAnnotee: Tree = try {
       import preprocessors._
       import components._
-      import retypecheck._
 
       val preprocessedAnnottee = Preprocessor.run(c)(
         annottee,
@@ -160,8 +150,6 @@ class Multitier(val c: blackbox.Context) {
 
       if (expandMultitierMacro) {
         try {
-          val retyper = c.retyper
-
           logging.info(
             s"Expanding multitier code for ${c.internal.enclosingOwner.fullName}.${annottee.name} " +
             s"in ${c.enclosingPosition.source.file.path}")

scala-loci-lang/shared/src/test/scala/loci/language/impl/MacroExpansionSpec.scala
@@ -393,6 +393,15 @@ class MacroExpansionSpec extends AnyFlatSpec with Matchers with NoLogging {
   }
 
   it should "typecheck early initializers" in {
+    // breaks Windows build due to http://github.com/scala/bug/issues/12498 and http://github.com/sbt/sbt/issues/6720
+
+    @platform(platform.win) object skip {
+      cancel("Skipping test: Does not compile on Windows due to scalac and sbt issues")
+    }
+
+    skip
+
+    @platform(!platform.win) object test {
     """@multitier trait mod extends TopModule {
       class C[T](v: T) { def z = top; type X = Int }
 
@@ -414,6 +423,9 @@ class MacroExpansionSpec extends AnyFlatSpec with Matchers with NoLogging {
       a.staticAssertType[C[Int] { val x: Int; type Y = Int; type T <: { type U }; def u(x: Int)(y: Int): Int}]
       b.staticAssertType[U { val x: Int; type Y = Int; type T <: { type U }; def u(x: Int)(y: Int): Int}]
     }""" should compile
+    }
+
+    test
   }
 
   it should "typecheck nested types" in {

.gitignore
@@ -8,6 +8,7 @@ target/
 .project
 .classpath
 .bloop
+.bsp
 *.iml
 *.ipr
 *.iws
@@ -22,4 +23,3 @@ target/
 *.out
 *.synctex.gz
 *.toc
-.bsp/

build.sbt
@@ -3,19 +3,23 @@ import sbtcrossproject.CrossPlugin.autoImport.{CrossType, crossProject}
 
 enablePlugins(GitVersioning)
 
-(ThisBuild / git.useGitDescribe) := true
+Global / excludeLintKeys += git.useGitDescribe
 
-(ThisBuild / scalaVersion) := "2.13.5"
+ThisBuild / git.useGitDescribe := true
 
-(ThisBuild / crossScalaVersions) := Seq("2.11.12", "2.12.11", "2.13.5")
+ThisBuild / organization := "io.github.scala-loci"
 
-(ThisBuild / organization) := "de.tuda.stg"
+ThisBuild / homepage := Some(url("https://scala-loci.github.io/"))
 
-(ThisBuild / licenses) += "Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")
+ThisBuild / licenses += "Apache-2.0" -> url("https://www.apache.org/licenses/LICENSE-2.0")
 
-(ThisBuild / scalacOptions) ++= Seq("-feature", "-deprecation", "-unchecked", "-Xlint", "-language:higherKinds")
+ThisBuild / scalacOptions ++= Seq("-feature", "-deprecation", "-unchecked", "-Xlint", "-language:higherKinds")
 
-(ThisBuild / resolvers) += ("STG old bintray repo" at "http://www.st.informatik.tu-darmstadt.de/maven/").withAllowInsecureProtocol(true)
+ThisBuild / resolvers += "jitpack" at "https://jitpack.io"
+
+ThisBuild / scalaVersion := "2.13.7"
+
+ThisBuild / crossScalaVersions := Seq("2.11.12", "2.12.15", "2.13.7")
 
 def `is 2.12+`(scalaVersion: String): Boolean =
   CrossVersion.partialVersion(scalaVersion) collect { case (2, n) => n >= 12 } getOrElse false
@@ -41,58 +45,44 @@ val macroparadise = Seq(
 val macrodeclaration = libraryDependencies +=
   scalaOrganization.value % "scala-reflect" % scalaVersion.value % Provided
 
-val jsweakreferences = Seq(
-  libraryDependencies ++= {
-    if ("0.6" != System.getenv("SCALAJS"))
-      Seq("org.scala-js" %%% "scalajs-fake-weakreferences" % "1.0.0")
-    else
-      Seq.empty
-  })
+val jsweakreferences = libraryDependencies += 
+  "org.scala-js" %%% "scalajs-weakreferences" % "1.0.0"
 
 val scalatest = libraryDependencies +=
-  "org.scalatest" %%% "scalatest" % "3.2.8" % Test
+  "org.scalatest" %%% "scalatest" % "3.2.10" % Test
 
 val scribe = libraryDependencies += {
   if (`is 2.12+`(scalaVersion.value))
-    "com.outr" %%% "scribe" % "3.6.2"
+    "com.outr" %%% "scribe" % "3.6.3"
   else
-    "com.outr" %%% "scribe" % "2.7.9"
+    "com.outr" %%% "scribe" % "3.6.2"
 }
-
-val retypecheckRepo =
-  resolvers += Resolver.bintrayRepo("stg-tud", "maven")
 
 val retypecheck = libraryDependencies +=
-  "de.tuda.stg" %% "retypecheck" % "0.7.0"
-
-val rescalaRepo =
-  resolvers += Resolver.bintrayRepo("stg-tud", "maven")
+  "io.github.scala-loci" %% "retypecheck" % "0.9.0"
 
 val rescala = libraryDependencies +=
-  "de.tuda.stg" %%% "rescala" % "0.30.0"
+  "com.github.rescala-lang.rescala" %%% "rescala" % "cbb7980"
 
-val upickle = libraryDependencies += {
-  if (`is 2.12+`(scalaVersion.value))
-    "com.lihaoyi" %%% "upickle" % "1.3.12"
-  else
-    "com.lihaoyi" %%% "upickle" % "0.7.4"
-}
+val upickle = libraryDependencies +=
+  "com.lihaoyi" %%% "upickle" % "1.4.2"
 
-val circe = libraryDependencies ++= {
-  if (`is 2.12+`(scalaVersion.value))
-    Seq(
-      "io.circe" %%% "circe-core" % "0.13.0",
-      "io.circe" %%% "circe-parser" % "0.13.0")
-  else
-    Seq(
-      "io.circe" %%% "circe-core" % "0.11.2",
-      "io.circe" %%% "circe-parser" % "0.11.2")
-}
+val circe = Seq(
+  libraryDependencies ++= {
+    if (`is 2.12+`(scalaVersion.value))
+      Seq(
+        "io.circe" %%% "circe-core" % "0.14.1",
+        "io.circe" %%% "circe-parser" % "0.14.1")
+    else
+      Seq.empty
+  },
+  (compile / skip) := !`is 2.12+`(scalaVersion.value),
+  (publish / skip) := !`is 2.12+`(scalaVersion.value))
 
 val jsoniter = Seq(
   libraryDependencies ++= {
     if (`is 2.12+`(scalaVersion.value))
-      Seq("com.github.plokhotnyuk.jsoniter-scala" %%% "jsoniter-scala-core" % "2.8.0")
+      Seq("com.github.plokhotnyuk.jsoniter-scala" %%% "jsoniter-scala-core" % "2.12.0")
     else
       Seq.empty
   },
@@ -108,10 +98,10 @@ val play = libraryDependencies ++= Seq(
   "com.typesafe.play" %% "play" % "[2.5,2.8)" % Provided)
 
 val scalajsDom = libraryDependencies +=
-  "org.scala-js" % "scalajs-dom" % "1.1.0" cross ScalaJSCrossVersion.binary
+  "org.scala-js" % "scalajs-dom" % "2.0.0" cross ScalaJSCrossVersion.binary
 
 val javalin = libraryDependencies +=
-  "io.javalin" % "javalin" % "3.13.7"
+  "io.javalin" % "javalin" % "4.1.1"
 
 val jetty = libraryDependencies ++= Seq(
   "org.eclipse.jetty.websocket" % "websocket-server" % "9.4.44.v20210927",
@@ -159,8 +149,7 @@ lazy val lociLang = (crossProject(JSPlatform, JVMPlatform)
   in file("scala-loci-lang")
   settings (normalizedName := "scala-loci-lang",
             SourceGenerator.remoteSelection,
-            retypecheckRepo, retypecheck,
-            macroparadise, macrodeclaration, scribe, scalatest)
+            retypecheck, macroparadise, macrodeclaration, scribe, scalatest)
   jsSettings jsweakreferences
   dependsOn lociCommunication % "compile->compile;test->test")
 
@@ -224,7 +213,7 @@ lazy val lociSerializerCirceJVM = lociSerializerCirce.jvm
 lazy val lociSerializerCirceJS = lociSerializerCirce.js
 
 
-lazy val lociSerializerJsoniterScala = (crossProject(JSPlatform, JVMPlatform) 
+lazy val lociSerializerJsoniterScala = (crossProject(JSPlatform, JVMPlatform)
   crossType CrossType.Pure
   in file("scala-loci-serializer-jsoniter-scala")
   settings (normalizedName := "scala-loci-serializer-jsoniter-scala",
@@ -239,7 +228,7 @@ lazy val lociTransmitterRescala = (crossProject(JSPlatform, JVMPlatform)
   crossType CrossType.Pure
   in file("scala-loci-transmitter-rescala")
   settings (normalizedName := "scala-loci-transmitter-rescala",
-            rescalaRepo, rescala, scalatest)
+            rescala, scalatest)
   dependsOn lociCommunication % "compile->compile;test->test")
 
 lazy val lociTransmitterRescalaJVM = lociTransmitterRescala.jvm
@@ -334,4 +323,3 @@ lazy val lociCommunicatorWebRtc = (crossProject(JSPlatform, JVMPlatform)
 
 lazy val lociCommunicatorWebRtcJVM = lociCommunicatorWebRtc.jvm
 lazy val lociCommunicatorWebRtcJS = lociCommunicatorWebRtc.js
-

jitpack.yml
@@ -1,0 +1,6 @@
+jdk:
+  - openjdk11
+install:
+  - sbt -Dsbt.log.noformat=true clean +publishM2
+env:
+  SBT_OPTS: -Xmx2048M

project/SourceGenerator.scala
@@ -3,7 +3,7 @@ import sbt._
 
 object SourceGenerator {
   val transmittableTuples =
-    sourceGenerators in Compile += sourceManaged in Compile map { dir =>
+    Compile / sourceGenerators += Compile / sourceManaged map { dir =>
       val members = (1 to 22) map { i =>
         val tuple = s"Tuple$i"
         val tupleArgsT = (0 until i) map { i => s"T$i" } mkString ", "
@@ -75,7 +75,7 @@ object SourceGenerator {
     }
 
   val remoteSelection =
-    sourceGenerators in Compile += sourceManaged in Compile map { dir =>
+    Compile / sourceGenerators += Compile / sourceManaged map { dir =>
       val members = (2 to 22) map { i =>
         val args = s"Remote[R]${", Remote[R]" * (i - 1)}"
         s"""  implicit def tuple$i[R](r: Tuple$i[$args]): RemoteSelection[R, fromMultiple] = erased
@@ -103,7 +103,7 @@ object SourceGenerator {
     }
 
   val functionsBindingBuilder =
-    sourceGenerators in Compile += sourceManaged in Compile map { dir =>
+    Compile / sourceGenerators += Compile / sourceManaged map { dir =>
       val builders = (0 to 22) map { i =>
         val argTypes = (0 until i) map { i => s"T$i" } mkString ", "
         val typedArgs = (0 until i) map { i => s"v$i: T$i" } mkString ", "
@@ -178,7 +178,7 @@ object SourceGenerator {
     }
 
   val functionSubjectiveBinding =
-    sourceGenerators in Compile += sourceManaged in Compile map { dir =>
+    Compile / sourceGenerators += Compile / sourceManaged map { dir =>
       val subjectiveBinding = (0 to 21) map { i =>
         val argTypes = (0 until i) map { i => s"T$i" } mkString ", "
         val typedArgs = (0 until i) map { i => s"v$i: T$i" } mkString ", "

project/build.properties
@@ -1,1 +1,1 @@
-sbt.version=1.5.2
+sbt.version=1.5.5

project/plugins.sbt
@@ -1,6 +1,6 @@
-libraryDependencies += "org.slf4j" % "slf4j-nop" % "1.7.30"
+libraryDependencies += "org.slf4j" % "slf4j-nop" % "1.7.32"
 
-addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject" % "1.0.0")
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % (if ("0.6" == System.getenv("SCALAJS")) "0.6.32" else "1.7.1"))
+addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject" % "1.1.0")
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.7.1")
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "1.0.0")
+addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "1.0.2")

commit 9e65445e7a6d90f26be0c35a7722ae830b48c3b3

fix issue with class arguments with default values (introduced in 1fc2f19)

language/shared/src/main/scala/loci/embedding/impl/Multitier.scala
@@ -98,7 +98,9 @@ class Multitier(val c: blackbox.Context) {
               _ map { tree =>
                 val ValDef(mods, name, tpt, rhs) = tree: @unchecked
                 if ((mods hasFlag Flag.DEFAULTPARAM) && rhs.nonEmpty)
-                  treeCopy.ValDef(tree, retyper.cleanModifiers(mods), name, tpt, EmptyTree)
+                  treeCopy.ValDef(tree,
+                    retyper.cleanModifiers(mods, removeFlags = Flag.DEFAULTINIT | Flag.DEFAULTPARAM),
+                    name, tpt, EmptyTree)
                 else
                   tree
               }

language/shared/src/test/scala/loci/embedding/MacroExpansionSpec.scala
@@ -197,6 +197,11 @@ class TopClass
 }
 
 
+@multitier class ByNameDefaultAndImplicitArguments(v: => Int = 1)(implicit ev: String) {
+  def res = ev * v
+}
+
+
 class MacroExpansionSpec extends AnyFlatSpec with Matchers with NoLogging {
   behavior of "Macro Expansion"
 
@@ -426,6 +431,31 @@ class MacroExpansionSpec extends AnyFlatSpec with Matchers with NoLogging {
     }
 
     platform(!platform.win) { test }
+  }
+
+  it should "correctly compile by-name arguments with default values and implicit arguments" in {
+    var count = 0
+    implicit val implicitString = "test"
+
+    val fixedInstance = new ByNameDefaultAndImplicitArguments
+    val fixedValues = new fixedInstance.`<placed values of loci.embedding.ByNameDefaultAndImplicitArguments>` {
+      lazy val $loci$sig = ""
+      def $loci$sys$create = emptySystem(this)
+    }
+
+    fixedValues.res should be ("test")
+    fixedValues.res should be ("test")
+    fixedValues.res should be ("test")
+
+    val countingInstance = new ByNameDefaultAndImplicitArguments({ count += 1; count })
+    val countingValues = new countingInstance.`<placed values of loci.embedding.ByNameDefaultAndImplicitArguments>` {
+      lazy val $loci$sig = ""
+      def $loci$sys$create = emptySystem(this)
+    }
+
+    countingValues.res should be ("test")
+    countingValues.res should be ("testtest")
+    countingValues.res should be ("testtesttest")
   }
 
   it should "typecheck nested types" in {