6a5b827619227f72c405a4bdef2601c33f49f726
Lukas Lehmann <lukas.lehmann97@gmail.com>
2022-01-01 17:41:28 +0100

    add useful functions to MonitorProduct API

--- scala-loci-lang/shared/src/main/scala/loci/NetworkMonitor.scala
+++ scala-loci-lang/shared/src/main/scala/loci/NetworkMonitor.scala
@@ -1,11 +1,39 @@
 package loci
 
 import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+import scala.concurrent.duration.Duration
 
 trait NetworkMonitor {
+
   def getStoredPings[P](remote: Remote[P]): Seq[PingInfo]
+
   def getSentBytes[P](remote: Remote[P]): Seq[TransmittedBytesInfo]
+
   def getReceivedBytes[P](remote: Remote[P]): Seq[TransmittedBytesInfo]
+
+  def sumSentBytes[P](remote: Remote[P], last: Duration = Duration.Inf): Long = sumBytes(getSentBytes(remote), last)
+
+  def sumReceivedBytes[P](remote: Remote[P], last: Duration = Duration.Inf): Long = sumBytes(getReceivedBytes(remote), last)
+
+  private def sumBytes[P](bytes: Seq[TransmittedBytesInfo], last: Duration): Long = {
+    val bytesInTimeRange = last match {
+      case Duration.Inf => bytes
+      case last =>
+        bytes.dropWhile(_.timestamp.isBefore(LocalDateTime.now().minus(last.toMillis, ChronoUnit.MILLIS)))
+    }
+    bytesInTimeRange.map(_.bytes.toLong).sum
+  }
+
+  def averagePing[P](remote: Remote[P], last: Duration = Duration.Inf): Double = {
+    val pingsInTimeRange = last match {
+      case Duration.Inf => getStoredPings(remote)
+      case last =>
+        getStoredPings(remote).dropWhile(_.timestamp.isBefore(LocalDateTime.now().minus(last.toMillis, ChronoUnit.MILLIS)))
+    }
+    pingsInTimeRange.map(_.ping).sum.toDouble / pingsInTimeRange.size
+  }
+
 }
 
 case class TransmittedBytesInfo(
