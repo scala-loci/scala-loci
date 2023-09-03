870b13ab10d6b8f422f98614c34d3e9f7a8da30f
ragnar <git@rmgk.de>
2023-09-03 11:11:37 +0200

    fix scala 2.11 build

--- communicator-ws-jetty12/jvm/src/main/scala/loci/communicator/ws/jetty/WSListener.scala
+++ communicator-ws-jetty12/jvm/src/main/scala/loci/communicator/ws/jetty/WSListener.scala
@@ -4,8 +4,11 @@ package ws.jetty
 
 import org.eclipse.jetty.util.Callback
 import org.eclipse.jetty.websocket.server
-import org.eclipse.jetty.websocket.server.{ServerUpgradeResponse, ServerWebSocketContainer, WebSocketCreator, WebSocketUpgradeHandler}
+import org.eclipse.jetty.websocket.server.{
+  ServerUpgradeResponse, ServerWebSocketContainer, WebSocketCreator, WebSocketUpgradeHandler
+}
 
+import java.util.function.Consumer
 import scala.util.{Failure, Success, Try}
 
 private class WSListener[P <: WS: WSProtocolFactory](
@@ -16,38 +19,42 @@ private class WSListener[P <: WS: WSProtocolFactory](
   self =>
 
   protected def startListening(connectionEstablished: Connected[P]): Try[Listening] = {
-    webSocketUpgradeHandler.configure { (wsContainer: ServerWebSocketContainer) =>
-      wsContainer.addMapping(
-        pathspec,
-        new WebSocketCreator {
-          override def createWebSocket(
-              request: server.ServerUpgradeRequest,
-              upgradeResponse: ServerUpgradeResponse,
-              callback: Callback
-          ): AnyRef = {
-            val uri = request.getHttpURI
-            val tls = uri.getScheme == "wss"
+    webSocketUpgradeHandler.configure {
+      new Consumer[ServerWebSocketContainer] {
+        override def accept(wsContainer: ServerWebSocketContainer): Unit = {
+          wsContainer.addMapping(
+            pathspec,
+            new WebSocketCreator {
+              override def createWebSocket(
+                  request: server.ServerUpgradeRequest,
+                  upgradeResponse: ServerUpgradeResponse,
+                  callback: Callback
+              ): AnyRef = {
+                val uri = request.getHttpURI
+                val tls = uri.getScheme == "wss"
 
-            implicitly[WSProtocolFactory[P]].make(
-              request.getHttpURI.toString,
-              Some(uri.getHost),
-              Some(uri.getPort),
-              self,
-              tls,
-              tls,
-              tls,
-              Some(request)
-            ) match {
-              case Failure(exception) =>
-                connectionEstablished.fire(Failure(exception))
-                null
+                implicitly[WSProtocolFactory[P]].make(
+                  request.getHttpURI.toString,
+                  Some(uri.getHost),
+                  Some(uri.getPort),
+                  self,
+                  tls,
+                  tls,
+                  tls,
+                  Some(request)
+                ) match {
+                  case Failure(exception) =>
+                    connectionEstablished.fire(Failure(exception))
+                    null
 
-              case Success(ws) =>
-                new Socket[P](ws, properties)(connectionEstablished.fire, Function.const(()))
+                  case Success(ws) =>
+                    new Socket[P](ws, properties)(connectionEstablished.fire, Function.const(()))
+                }
+              }
             }
-          }
+          )
         }
-      )
+      }
     }
 
     Success(new Listening {
